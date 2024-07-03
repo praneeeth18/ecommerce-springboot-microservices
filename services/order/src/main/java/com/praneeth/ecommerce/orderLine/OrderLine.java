545141b46341c05d7f75ab2255e67116e4aa23cb
Praneeth Reddy <praneeth608@gmail.com>
2024-07-03 14:56:29 +0530

    order configuration and entities created

---
+++ services/order/src/main/java/com/praneeth/ecommerce/orderLine/OrderLine.java
@@ -0,0 +1,23 @@
+package com.praneeth.ecommerce.orderLine;
+
+import com.praneeth.ecommerce.order.Order;
+import jakarta.persistence.*;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+public class OrderLine {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+    private Integer productId;
+    private double quantity;
+}
