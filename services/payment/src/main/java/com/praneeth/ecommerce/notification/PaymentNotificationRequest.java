178049e7274e53a8b4b7c0156ebb6df99f16c439
Praneeth Reddy <praneeth608@gmail.com>
2024-07-09 17:04:59 +0530

    payment service implementation

---
+++ services/payment/src/main/java/com/praneeth/ecommerce/notification/PaymentNotificationRequest.java
@@ -0,0 +1,15 @@
+package com.praneeth.ecommerce.notification;
+
+import com.praneeth.ecommerce.payment.PaymentMethod;
+
+import java.math.BigDecimal;
+
+public record PaymentNotificationRequest(
+        String orderReference,
+        BigDecimal amount,
+        PaymentMethod paymentMethod,
+        String customerFirstName,
+        String customerLastName,
+        String customerEmail
+) {
+}
---
+++ services/payment/src/main/java/com/praneeth/ecommerce/payment/PaymentService.java
@@ -0,0 +1,32 @@
+package com.praneeth.ecommerce.payment;
+
+import com.praneeth.ecommerce.notification.NotificationProducer;
+import com.praneeth.ecommerce.notification.PaymentNotificationRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentService {
+
+    private final PaymentRepository repository;
+    private final PaymentMapper mapper;
+    private final NotificationProducer notificationProducer;
+
+    public Integer createPayment(PaymentRequest request) {
+        var payment = repository.save(mapper.toPayment(request));
+
+        notificationProducer.sendNotification(
+                new PaymentNotificationRequest(
+                        request.orderReference(),
+                        request.amount(),
+                        request.paymentMethod(),
+                        request.customer().firstname(),
+                        request.customer().lastname(),
+                        request.customer().email()
+                )
+        );
+        return payment.getId();
+    }
+
+}
