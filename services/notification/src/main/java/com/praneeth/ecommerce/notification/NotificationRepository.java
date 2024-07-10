81e814aeac5efa521d34988630d6623f10933c7b
Praneeth Reddy <praneeth608@gmail.com>
2024-07-10 11:49:29 +0530

    notification consumer implemented

--- services/notification/src/main/java/com/praneeth/ecommerce/notification/NotificationRepository.java
+++ services/notification/src/main/java/com/praneeth/ecommerce/notification/NotificationRepository.java
@@ -1,7 +1,6 @@
 package com.praneeth.ecommerce.notification;
 
-import com.praneeth.ecommerce.kafka.payment.PaymentConfirmation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface NotificationRepository extends MongoRepository<PaymentConfirmation, String> {
+public interface NotificationRepository extends MongoRepository<Notification, String> {
 }
